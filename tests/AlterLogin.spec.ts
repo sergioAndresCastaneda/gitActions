61003c801f5b96f8b62c459ed86cc489585272f8
Sergio Castaneda <seancadi@gmail.com>
2025-04-24 16:23:18 -0500

    Captura captcha

--- tests/AlterLogin.spec.ts
+++ tests/AlterLogin.spec.ts
@@ -5,10 +5,10 @@ import credentialsData from './data/credentials2.json';
 
 credentialsData.forEach((user) => {
     test(`Test de ${user.name}`, async ({ page }) => {
-        const login = new loginPage(page);
-        await page.screenshot({ path: 'captcha-error.png', fullPage: true });       
+        const login = new loginPage(page);    
         await page.goto(`${user.path}`);
         await login.loginWhitCredentials(user.username, user.password);
+        await page.screenshot({ path: 'captcha-error.png', fullPage: true }); // Captura de pantalla del error de captcha
         await takeScreenshot(page, `Screenshot de ${user.screenshot}`);
         await page.close();
     });
