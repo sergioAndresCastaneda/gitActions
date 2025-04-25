b5adf5ae32e76a572f5fb6197b0a7db25f6064f3
Sergio Castaneda <seancadi@gmail.com>
2025-04-25 12:32:09 -0500

    Remover headless

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,7 +25,6 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    headless: false,
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'https://t-banking-bo.novopayment.net',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
